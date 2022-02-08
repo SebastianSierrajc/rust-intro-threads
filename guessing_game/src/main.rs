bce778c73c3f2f8b9e1117c087a64a09198c9c3b
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-02-08 00:36:14 -0500

    guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    let secret_num = rand::thread_rng().gen_range(1..101);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("\nPlease input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim() == "quit" {
+            return;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); 
+                break;
+            },
+        }
+   }
+}
