0323d0cd6d38531eb437a8ad6642fe83be6797b0
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-02-07 22:57:49 -0500

    remove bins

--- hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e.d
+++
@@ -1,5 +0,0 @@
-/home/sebastian/Backup/usa/rust-intro-threads/hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e: src/main.rs
-
-/home/sebastian/Backup/usa/rust-intro-threads/hello-rust/target/debug/deps/hello_rust-7d0160304e5a313e.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/debug/hello-rust.d
+++
@@ -1,1 +0,0 @@
-/home/sebastian/Backup/usa/rust-intro-threads/hello-rust/target/debug/hello-rust: /home/sebastian/Backup/usa/rust-intro-threads/hello-rust/src/main.rs
